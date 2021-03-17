fd05b176d692bded527f9780d4a871715701e465
ikonikbiz <80822668+ikonikbiz@users.noreply.github.com>
2021-03-17 17:24:00 +0100

    Add files via upload
    ipay website

---
+++ simple_style 2/popper.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from themextar.net/demos/appli-shihab/all_pages/simple_style/popper.js by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Mar 2021 12:33:40 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ThemeXtar is under construction</title>
+    <meta name="description" content="Creative Digital Web Agency &amp; A Group of Internet Technology" />
+    <meta name="generator" content="Free UnderConstructionPage plugin for WordPress">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,900">
+    <link rel="stylesheet" href="http://themextar.net/wp-content/plugins/under-construction-page/themes/css/bootstrap.min.css?v=3.45" type="text/css">
+<link rel="stylesheet" href="http://themextar.net/wp-content/plugins/under-construction-page/themes/css/common.css?v=3.45" type="text/css">
+<link rel="stylesheet" href="http://themextar.net/wp-content/plugins/under-construction-page/themes/mad_designer/style.css?v=3.45" type="text/css">
+<link rel="stylesheet" href="http://themextar.net/wp-content/plugins/under-construction-page/themes/css/font-awesome.min.css?v=3.45" type="text/css">
+<link rel="shortcut icon" type="image/png" href="http://themextar.net/wp-content/plugins/under-construction-page/themes/images/favicon.png" />
+  </head>
+
+  <body>
+    <div id="hero-image">
+      <img src="http://themextar.net/wp-content/plugins/under-construction-page/themes/mad_designer/mad-designer.png" alt="Mad Designer at work" title="Mad Designer at work">
+    </div>
+    <div class="container">
+
+      <div class="row">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+          <h1>Sorry, we're doing some work on the site</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-md-8 col-md-offset-2 col-lg-offset-2 col-lg-8">
+          <p class="content">Thank you for being patient. We are doing some work on the site and will be back shortly.<br />
+Our Mail: <a href="mailto:mdmahinurkhan9@gmail.com">ThemeXtar</a><br />
+Our Facebook: <a href="https://fb.me/themextarbymprotech" target="_blank" rel="noopener">ThemeXtar</a></p>
+        </div>
+      </div>
+
+      <div class="row" id="social">
+        <div class="col-xs-12 col-md-12 col-lg-12">
+          
+        </div>
+      </div>
+
+    </div>
+    <div id="login-button" class="loggedout"><a title="Log in to WordPress admin" href="http://themextar.net/wp-login.php"><i class="fa fa-wordpress fa-2x" aria-hidden="true"></i></a></div>
+  </body>
+
+<!-- Mirrored from themextar.net/demos/appli-shihab/all_pages/simple_style/popper.js by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Mar 2021 12:33:40 GMT -->
+</html>
